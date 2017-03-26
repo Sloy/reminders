d30d219273cd5194e379695eff591dca9df117fc
Rafa Vázquez <rafa.vazsan@gmail.com>
2017-03-26 14:01:45 +0200

    Simplify firebase extensions

--- app/src/main/java/com/sloydev/remember/data/FirebaseExtensions.kt
+++ app/src/main/java/com/sloydev/remember/data/FirebaseExtensions.kt
@@ -1,6 +1,5 @@
 package com.sloydev.remember.data
 
-import android.util.Log
 import com.google.firebase.database.DataSnapshot
 import com.google.firebase.database.DatabaseError
 import com.google.firebase.database.DatabaseReference
@@ -14,25 +13,14 @@ suspend fun DatabaseReference.getValue(): DataSnapshot {
     return async(CommonPool) {
         suspendCoroutine<DataSnapshot> { continuation ->
             addListenerForSingleValueEvent(FValueEventListener(
-                    onDataChange = { data ->
-                        Log.d("firebase", "onDataChange")
-                        continuation.resume(data)
-                    },
-                    onError = { error ->
-                        Log.d("firebase", "onError")
-                        continuation.resumeWithException(error.toException())
-                    }
+                    onDataChange = { continuation.resume(it) },
+                    onError = { continuation.resumeWithException(it.toException()) }
             ))
         }
     }.await()
 }
 
 class FValueEventListener(val onDataChange: (DataSnapshot) -> Unit, val onError: (DatabaseError) -> Unit) : ValueEventListener {
-    override fun onDataChange(data: DataSnapshot) {
-        onDataChange.invoke(data)
-    }
-
-    override fun onCancelled(error: DatabaseError) {
-        onError.invoke(error)
-    }
+    override fun onDataChange(data: DataSnapshot) = onDataChange.invoke(data)
+    override fun onCancelled(error: DatabaseError) = onError.invoke(error)
 }
